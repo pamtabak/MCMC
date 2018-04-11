aa0454f5285d7f2043b71091edbcfce8c5b0ae07
Pamela Tabak <pamelatabak@poli.ufrj.br>
2018-04-10 21:29:00 -0300

    inverse_transformation: adding matplotlib

--- inverse_transformation.py
+++ inverse_transformation.py
@@ -1,25 +1,38 @@
 import random
 import math
+import matplotlib.pyplot as plt
 
-def exponential_function (random_uniform):
+def exponential_function ():
 	lambda_value = 3
-	return (math.log(lambda_value) - math.log(random_uniform)/lambda_value)
+	x = []
+	y = []
+	for i in range(0, 200):
+		random_number = random.uniform(0, 1)
+		y.append(random_number)
+		x.append(math.log(lambda_value) - math.log(random_number)/lambda_value)
+	plt.plot(x, y, 'ro')
+	plt.ylabel('Uniforme(0,1)')
+	plt.xlabel('Valor da Inversa')
+	plt.show()
 
 def pareto_function ():
 	alpha_value = 5.0
 	first_x     = 1.0
-	x 			= 0
 
-	y = random.uniform(0, 1)
-	while (x < first_x):
-		x = ((alpha_value*(first_x**alpha_value))/y)**(1/(alpha_value + 1))
+	x = []
+	y = []
+	for i in range(0, 200):
+		x_value 	  = 0
+		random_number = random.uniform(0, 1)
+		y.append(random_number)
+		while (x_value < first_x):
+			x_value = ((alpha_value*(first_x**alpha_value))/random_number)**(1/(alpha_value + 1))
+		x.append(x_value)
+	plt.plot(x, y, 'bo')
+	plt.ylabel('Uniforme(0,1)')
+	plt.xlabel('Valor da Inversa')
+	plt.show()
 
-	return x
-	
 
-result = ""
-for i in range(0,20):
-	#result += str(exponential_function(random.uniform(0, 1))) + ", "
-	result += str(pareto_function()) + ", "
-
-print(result)
+#exponential_function()
+pareto_function()
