fefbc63c02ccacf033020322a490e0db06bf41dd
Pamela Tabak <pamelatabak@poli.ufrj.br>
2018-05-20 12:42:08 -0300

    minor changes

--- mixing_time.py
+++ mixing_time.py
@@ -47,7 +47,6 @@ def generate_binary_tree (number_of_nodes, exponential):
 	return matrix
 
 def prepare_binary_tree_data (number_of_nodes, exponential):
-	#preparing data
 	states = []
 	transitions = []
 	pi = []
@@ -81,13 +80,14 @@ def create_grid_2d (number_of_nodes):
 	for i in range(0, number_of_nodes):
 		matrix.append([])
 	lines = []
-	for i in range(0, math.sqrt(number_of_nodes)):
+	number_of_lines = int(math.sqrt(number_of_nodes))
+	for i in range(0, number_of_lines):
 		line = []
-		for j in range(0, math.sqrt(number_of_nodes)):
-			line.append(i*math.sqrt(number_of_nodes) + j)
-			if (j < (math.sqrt(number_of_nodes) - 1)):
-				matrix[i*math.sqrt(number_of_nodes) + j].append(i*math.sqrt(number_of_nodes) + j + 1)
-				matrix[i*math.sqrt(number_of_nodes) + j + 1].append(i*math.sqrt(number_of_nodes) + j)
+		for j in range(0, number_of_lines):
+			line.append(i*number_of_lines + j)
+			if (j < (number_of_lines - 1)):
+				matrix[i*number_of_lines + j].append(i*number_of_lines + j + 1)
+				matrix[i*number_of_lines + j + 1].append(i*number_of_lines + j)
 		lines.append(line)
 	for l in range(0, len(lines) - 1):
 		for element in range(0, len(lines[l])):
@@ -99,7 +99,8 @@ def prepare_grid_data (number_of_nodes):
 	states = []
 	transitions = []
 	pi = []
-	grid = create_grid_2d()
+	grid = create_grid_2d (number_of_nodes)
+	number_of_edges = 0
 	for i in range (0, number_of_nodes):
 		states.append(i)
 		pi.append(0)
@@ -108,6 +109,7 @@ def prepare_grid_data (number_of_nodes):
 			if (i == j):
 				prob.append(0.5)
 			elif (j in grid[i]):
+				number_of_edges += 1
 				if (len(grid[i]) == 2):
 					prob.append(0.25)
 				if (len(grid[i]) == 3):
@@ -117,7 +119,7 @@ def prepare_grid_data (number_of_nodes):
 			else:
 				prob.append(0)
 		transitions.append(prob)
-	return (states, transitions, pi, grid)
+	return (states, transitions, pi, grid, number_of_edges)
 
 def run_ring_markov_chain (number_of_nodes):
 	variation   = 1.0
@@ -131,13 +133,13 @@ def run_ring_markov_chain (number_of_nodes):
 	transitions  = ring_data[1]
 	pi           = ring_data[2]
 	pi[0]        = 1
-	current_node = 0
+	#current_node = 0
 
 	while (variation > 0.000001):
 		mixing_time += 1
-		next_node = np.random.choice(states,replace=True,p=transitions[current_node])
+		#next_node = np.random.choice(states,replace=True,p=transitions[current_node])
 		pi = np.matmul(pi, transitions)
-		current_node = next_node
+		#current_node = next_node
 		v = 0
 		for i in range (0, len(pi)):
 			v += math.fabs ((stationary_distribution[i] - pi[i]))
@@ -154,53 +156,64 @@ def run_binary_tree_markov_chain (number_of_nodes):
 	variation   			= 1.0
 	stationary_distribution = []
 	mixing_time 			= 0
-	current_node 			= number_of_nodes - 1 #we are starting at last node
-	print(data[4])
+	#current_node 			= number_of_nodes - 1 #we are starting at last node
 	for i in range(0, number_of_nodes):
 		stationary_distribution.append(len(binary_tree_data[i])*1.0/data[4])
-	while (variation > 0.000001):
+	while (variation >= 0.000001):
 		mixing_time += 1
-		next_node = np.random.choice(states,replace=True,p=transitions[current_node])
+		#next_node = np.random.choice(states,replace=True,p=transitions[current_node])
 		pi = np.matmul(pi, transitions)
-		current_node = next_node
+		#current_node = next_node
 		v = 0
 		for i in range (0, len(pi)):
 			v += math.fabs ((stationary_distribution[i] - pi[i]))
 		variation = v/2
 	return mixing_time
 
-# def run_grid_markov_chain (time):
-# 	data = prepare_grid_data()
-# 	states = data[0]
-# 	transitions = data[1]
-# 	first_pi = data[2]
-# 	grid = data[3]
-# 	variation = []
-# 	stationary_distribution = []
-# 	# precisa pegar numero de arestas
-# 	for i in range(0, 1024):
-# 		stationary_distribution.append(len(grid[i])*1.0/3968)
-# 	for t in time:
-# 		print("working at " + str(t))
-# 		pi = grid_2d (t, states, transitions, first_pi, grid)
-# 		v = 0
-# 		for i in range (0, len(pi)):
-# 			v += math.fabs ((stationary_distribution[i] - pi[i]))
-# 		v = v/2
-# 		variation.append(v)
-# 		print(v)
-# 	return variation
+def run_grid_markov_chain (number_of_nodes):
+	data = prepare_grid_data(number_of_nodes)
+	states = data[0]
+	transitions = data[1]
+	pi = data[2]
+	grid = data[3]
+	number_of_edges = data[4]
+	variation = 0.0
+	mixing_time = 0
+	stationary_distribution = []
+	for i in range(0, number_of_nodes):
+		stationary_distribution.append(len(grid[i])*1.0/number_of_edges)
+	while (variation >= 0.000001):
+		mixing_time += 1
+		#next_node = np.random.choice(states,replace=True,p=transitions[current_node])
+		pi = np.matmul(pi, transitions)
+		#current_node = next_node
+		v = 0
+		for i in range (0, len(pi)):
+			v += math.fabs ((stationary_distribution[i] - pi[i]))
+		variation = v/2
+	return mixing_time
 
-# ring_number_of_nodes = [10, 50, 100, 300, 700, 1000, 3000, 5000, 10000]
-# ring_mixing_time = []
-# for n in ring_number_of_nodes:
-# 	ring_mixing_time.append(run_ring_markov_chain(n))
-# tree_number_of_nodes = [7, 15, 63, 127, 255, 511, 1023, 2047, 4095, 8191]
-tree_number_of_nodes = [1023]
+ring_number_of_nodes = [10, 50, 100, 300, 700, 1000, 3000, 5000, 10000]
+ring_mixing_time = []
+print("RING!!")
+for n in ring_number_of_nodes:
+	print(n)
+	ring_mixing_time.append(run_ring_markov_chain(n))
+tree_number_of_nodes = [7, 15, 63, 127, 255, 511, 1023, 2047, 4095, 8191]
+print ("TREE!!")
 tree_mixing_time = []
 for n in tree_number_of_nodes:
-	tree_mixing_time.append(run_binary_tree_markov_chain(n))
-plt.plot(mixing_time, number_of_nodes, 'ro')
+	print(n)
+ 	tree_mixing_time.append(run_binary_tree_markov_chain(n))
+grid_number_of_nodes = [8, 16, 64, 128, 256, 512, 1024, 2048, 4096, 8192]
+print("GRID!!")
+grid_mixing_time = []
+for n in grid_number_of_nodes:
+	print(n)
+	grid_mixing_time.append(run_grid_markov_chain(n))
+plt.plot(ring_mixing_time, ring_number_of_nodes, 'ro')
+plt.plot(tree_mixing_time, tree_number_of_nodes, 'go')
+plt.plot(grid_mixing_time, grid_number_of_nodes, 'bo')
 plt.xlabel('Tempo de Mistura')
 plt.ylabel('Numero de Nos')
 plt.show()
